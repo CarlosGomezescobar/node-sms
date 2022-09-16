a80a1e1e3c5dac6c17466f085ba887ed7bee80e6
CarlosGomezescobar <gg.20twenty1@gmail.com>
2022-09-16 05:48:31 -0400

    node-sms

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const http = require('http');
+const app = require('./server');
+const server = http.createServer(app);
+
+require('./database');
+require('./sockets').connection(server);
+
+// server.listen(app.get('port'), () => {
+// 	console.log('Server on port', app.get('port'));
+// })
+
+
+
+server.listen(3000);
+console.log("Server on port", 3000);
